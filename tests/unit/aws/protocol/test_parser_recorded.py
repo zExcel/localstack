cf73b53a250ac45f0e8e9c6aee2a6d374b688dd6
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-02-10 15:11:32 +0100

    fix datetime and none handling for recorder test

--- tests/unit/aws/protocol/test_parser_recorded.py
+++ tests/unit/aws/protocol/test_parser_recorded.py
@@ -2,8 +2,9 @@ import os
 import pickle
 import pprint
 from collections import defaultdict
+from datetime import datetime
 from functools import lru_cache
-from typing import Dict, TypedDict
+from typing import Callable, Dict, TypedDict, Union
 from urllib.parse import urlsplit
 
 import botocore.client
@@ -96,11 +97,64 @@ def test_parse_request(api_call: ApiCall):
     op, parsed = parser.parse(http_request)
 
     assert op.name == api_call["operation"]
+
     # align parsed and recorded parameters
-    # TODO: more alignment will have to be done)
     if parsed is None:
         parsed = {}
+    walk(parsed, delete_none_walker)
+    walk(parsed, delete_utc_walker)
+
     assert parsed == api_call["params"]
+
+
+def delete_utc_walker(target: Union[dict, list], key: any, value: any) -> bool:
+    if isinstance(value, datetime):
+        fixed_datetime = value.replace(tzinfo=None)
+        if isinstance(target, dict):
+            target[key] = fixed_datetime
+        elif isinstance(target, list):
+            for i, value_in_list in enumerate(target):
+                if value == value_in_list:
+                    target[i] = fixed_datetime
+
+    return True
+
+
+def delete_none_walker(target: Union[dict, list], key: any, value: any) -> bool:
+    if isinstance(target, dict) and value is None:
+        del target[key]
+        return False
+    elif isinstance(target, list) and value is None:
+        target.remove(value)
+        return False
+    return True
+
+
+def walk(target: Union[dict, list], fct: Callable) -> None:
+    if isinstance(target, dict):
+        for key, value in list(target.items()):
+            if fct(target, key, value):
+                walk(value, fct)
+    elif isinstance(target, list):
+        for item in target:
+            if fct(target, None, item):
+                walk(item, fct)
+
+
+def delete_none(target: Union[dict, list]) -> None:
+    """Delete None elements in-place recursively in dicts and lists."""
+    if isinstance(target, dict):
+        for key, value in list(target.items()):
+            if value is None:
+                del target[key]
+            else:
+                delete_none(value)
+    elif isinstance(target, list):
+        for item in target:
+            if item is None:
+                target.remove(item)
+            else:
+                delete_none(item)
 
 
 def to_aws_request(api_call: ApiCall) -> AWSPreparedRequest:
