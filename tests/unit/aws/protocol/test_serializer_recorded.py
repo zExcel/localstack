a801c561b1fff9b3bd2f32c99079fbec17134664
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-02-21 17:35:17 +0100

    fixup! fix exception class name lookup for error serialization challenge

--- tests/unit/aws/protocol/test_serializer_recorded.py
+++ tests/unit/aws/protocol/test_serializer_recorded.py
@@ -120,6 +120,6 @@ def test_serialize_response(api_call: ApiResponse):
     assert "RequestId" in parsed_response["ResponseMetadata"]
     assert len(parsed_response["ResponseMetadata"]["RequestId"]) == 52
     # There might be additional top-level members which have been parsed but were not in the initial data
-    # (like ETag fields for S3). Remove all data that is in the parsed_response but _not_ in the initial data.
-    parsed_response = {key: value for key, value in parsed_response.items() if key in data}
-    assert parsed_response == data
+    # (like ETag fields for S3).
+    # Therefore check if the parsed_response is a superset of data.
+    assert dict(parsed_response, **data) == parsed_response
